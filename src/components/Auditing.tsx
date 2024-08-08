3557cc2ee340fb2396db0ef8ffcc42bece2fc6d4
ygsdyu jhdbhj <shubhangigawande121@gmail.com>
2024-08-08 13:53:51 +0530

    updated files

--- src/components/Auditing.tsx
+++ src/components/Auditing.tsx
@@ -7,9 +7,9 @@ export function Auditing() {
         <p>Introduction AI - Auditing Learn more</p>
       </div>
 
-      <div className='container mx-auto h-15 bg-black flex items-center justify-between text-white p-4 mt-5'>
+      <div className='container mx-auto h-15 bg-black flex items-center justify-between text-white p-4'>
         <div className='flex items-center'>
-          <img src='/images/image.png' className='h-10 mr-3'/>
+          <img src='/images/image.png' className='h-10 mr-3' />
           <h1>Auditing</h1>
         </div>
         <button className='bg-green-400 text-white px-4 py-2 rounded hover:bg-green-600'>
@@ -17,7 +17,7 @@ export function Auditing() {
         </button>
       </div>
 
-      <div className='container mx-auto bg-blue-950 text-white p-6 mt-5'>
+      <div className='relative container mx-auto bg-blue-950 text-white p-6 h-[31rem]'>
         <div className='ml-52 text-left'>
           <h2 className='text-1xl'>Provable Trust For All</h2>
           <h1 className='text-5xl font-bold mt-8'>
@@ -29,25 +29,145 @@ export function Auditing() {
           </p>
 
           <div className='flex items-center mt-6'>
-            <input type='text' placeholder='Search Project by Token/ Contact address' className='w-full max-w-md p-2 rounded-l  bg-gray-300 text-center'/>
-            <button className='bg-green-400 text-white p-2 rounded-r hover:bg-green-600 w-12'> <i className='fas fa-search'></i>
+            <input type='text' placeholder='Search Project by Token/ Contact address' className='w-full max-w-md p-2 rounded-l bg-gray-300 text-center' />
+            <button className='bg-green-400 text-white p-2 rounded-r hover:bg-green-600 w-12'>
+              <i className='fas fa-search'></i>
             </button>
           </div>
-          
         </div>
+
+        <div className='flex space-x-8 mt-8 ml-52'>
+          <div className='relative border border-spacing-1 bg-white text-black w-72 h-96 rounded-lg border-black shadow-lg'>
+            <div className='mt-3'>
+              <div className='flex items-center'>
+                <img src='/images/m1.png' className='h-10 mr-3' />
+                <h1 className='font-bold text-lg'>Code Security Audits</h1>
+              </div>
+              <h2 className='ml-3 text-lg'>Smart Contract Audit</h2>
+
+              <div className='flex items-center w-10 h-6 pt-6'>
+                <img src='/images/m2.png' />
+                <img src='/images/m3.png' />
+                <img src='/images/m4.png' />
+                <img src='/images/m5.png' />
+                <img src='/images/m6.png' />
+              </div>
+
+              <div className='mt-11 bg-gradient-to-tl from-gray-500 to-white w-64 h-56 rounded-lg shadow-lg ml-3'>
+                <div className='ml-6 mt-4'>
+                  <h1 className='font-semibold text-base mt-8'>
+                    SMART<br />CONTRACT
+                  </h1>
+                  <p className='text-xs mt-2'>
+                    Lorem ipsum, dolor sit amet atque consectetur. ipsa doloremque quis magnam asperiores, sapiente alias veritatis laborum velit molestiae.
+                  </p>
+                  <button className=' w-28 h-7 bg-gradient-to-r mt-6 from-blue-500 to-sky-500 text-white text-sm rounded-lg hover:from-blue-700 hover:to-sky-700'>
+                    GET STARTED
+                  </button>
+                  <div className='flex items-center w-10 h-6 pt-6 mb-8'>
+                    <img src='/images/m7.png' />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='bg-slate-50 w-60 h-28 border rounded-lg'>
+            <div className='flex items-center ml-4 mt-2 '>
+              <img src='/images/m8.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>TrustWorthy</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>Security Score & Ranking</p>
+          </div>
+
+
+          <div className='bg-slate-50 w-60 h-28 border rounded-lg'>
+            <div className='flex items-center ml-4 mt-2 '>
+              <img src='/images/m8.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>Sky</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>Compliance, AML and Risk Management</p>
+          </div>
+
+
+        </div>
+
+        <div className='flex space-x-10 mt-8 ml-52 '>
+
+          <div className='bg-slate-50 w-60 h-28 border rounded-lg ml-80 -mt-72'>
+            <div className='flex items-center ml-4 mt-2 '>
+              <img src='/images/m9.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>Secure</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>Fully Secure and Responsible</p>
+          </div>
+
+
+          <div className='bg-slate-50 w-60 h-28 border rounded-lg ml-80 -mt-72'>
+            <div className='flex items-center ml-4 mt-2 '>
+              <img src='/images/m10.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>Pen Testing</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>In-Depth Attact Situation</p>
+          </div>
+        </div>
+
+
+        <div className='flex space-x-11 mt-32 ml-52 '>
+
+          <div className='bg-slate-50 w-60 h-28 border rounded-lg ml-80 -mt-72'>
+            <div className='flex items-center ml-4 mt-2 '>
+              <img src='/images/m11.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>Bug Bounty</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>Open Source bug detection</p>
+          </div>
+
+
+          <div className='bg-slate-50 w-60 h-28 border rounded-lg ml-80 -mt-72'>
+            <div className='flex items-center ml-4 mt-2 '>
+              <img src='/images/m12.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>Formal testing</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>Web3 Mathematics Guarantees</p>
+          </div>
+        </div>
+
+
+
+
       </div>
 
-    <div>
-      <div className='container border border-spacing-1 bg-white text-black w-56 h-64 ml-80 rounded-lg border-black'>
-        <div className='mt-3'>
-        <div className='flex items-center'>
-          <img src='/images/m1.png' className='h-10 mr-3'/>
-          <h1 className='font-bold'>Auditing</h1>
+
+
+
+      <div className='container mx-auto h-[31rem] bg-white justify-center '>
+
+
+        <div className='flex space-x-8 container'>
+          <div className='container mx-auto h-15 flex items-center justify-between p-4 mt-56 ml-20'>
+            <div className='flex items-center'>
+
+              <h1 className='text-5xl font-bold'>Testimonials</h1>
+            </div>
+            <button className='bg-green-400 font-semibold text-[1rem] text-white px-4 py-2 rounded hover:bg-green-600 ml-9 mr-24'>
+              Intrested In AI Audit.?
+            </button>
+          </div>
+
+
+
+          
         </div>
-        </div>
-       
-        </div>
-        </div>
+
+        <div className='bg-slate-50 w-60 h-28 border rounded-lg ml-20'>
+            <div className='flex items-center mt-2 '>
+              <img src='/images/m8.png' className='h-10 mr-3' />
+              <h1 className='font-semibold text-lg text-black'>TrustWorthy</h1>
+            </div>
+            <p className='text-gray-700  ml-4 mt-5 text-xs'>Security Score & Ranking</p>
+          </div>
+      </div>
     </div>
   );
 }
